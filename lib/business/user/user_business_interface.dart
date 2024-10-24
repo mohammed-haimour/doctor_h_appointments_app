d2d51b56e77c8c0579fde8cfb88d073a41c40090
moahmmed mahmoud <honhone123@gmail.com>
2024-10-24 21:56:21 +0300

    solving marge conflicts error and shits

--- lib/business/user/user_business_interface.dart
+++ lib/business/user/user_business_interface.dart
@@ -1,5 +1,5 @@
 import 'package:dartz/dartz.dart';
-import 'package:doctor_h_appointments_app/business/user/entities/login/create_account_result_entity.dart';
+import 'package:doctor_h_appointments_app/business/user/entities/create_account/create_account_result_entity.dart';
 import 'package:doctor_h_appointments_app/business/user/entities/login/login_result_entity.dart';
 import 'package:doctor_h_appointments_app/data/user/models/create_account/create_account_payload_model.dart';
 import 'package:doctor_h_appointments_app/data/user/models/login/login_payload_model.dart';
